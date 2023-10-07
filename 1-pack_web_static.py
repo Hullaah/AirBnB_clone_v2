a7e414533401e84a5e85d6622321f0d0c07b8224
Hullaah <umaradelo1.247@gmail.com>
2023-10-07 17:47:25 +0100

    Fixed pycodestyle issue

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone"""
+"""generates a .tgz archive from the contents of the web_static
+folder of your AirBnB Clone"""
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
-    """generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone"""
+    """generates a .tgz archive from the contents of the web_static
+    folder of your AirBnB Clone"""
     now = str(datetime.now())
     now = now[:now.find('.')]
     date = "".join([x for x in now if x.isdigit()])
