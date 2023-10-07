d28944d80643ad35783d6292df988e97b9288ad5
Hullaah <umaradelo1.247@gmail.com>
2023-10-07 13:44:40 +0100

    Solved task 1 of deploy

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# A Bash script that sets up your web servers for the deployment of web_static
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/server_name _;/a \\n\tlocation \/hbnb_static\/ { alias \/data\/web_static\/current\/; }' /etc/nginx/sites-available/default
+sudo service nginx restart
