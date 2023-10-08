938358abbab8e03ad813f9db2addd83635957b7f
Hullaah <umaradelo1.247@gmail.com>
2023-10-08 10:05:59 +0100

    Corrected task 2 and 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -34,9 +34,13 @@ def do_deploy(archive_path):
         archive_file,
         archive_file[:-4]))
     run("sudo rm -rf /tmp/{}".format(archive_file))
+    run("sudo mv /data/web_static/releases/" + archive_file[:-4] + "/web_static/*" +
+        " /data/web_static/releases/{}".format(archive_file[:-4]))
+    run("sudo rm -rf /data/web_static/releases/{}/web_static/".format(
+        archive_file[:-4]))
     run("sudo rm -rf /data/web_static/current")
     run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
-        .format(archive_file))
+        .format(archive_file[:-4]))
     print("New version deployed!")
     return True
 
