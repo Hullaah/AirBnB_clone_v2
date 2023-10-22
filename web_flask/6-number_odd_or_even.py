e3e28c714e32076f6469aff9faf6c071509df526
Hullaah <umaradelo1.247@gmail.com>
2023-10-22 20:54:01 +0100

    Corrected some errors in the file 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -47,8 +47,8 @@ def number_template(n):
 @app.route("/number_odd_or_even/<n>", strict_slashes=False)
 def number_odd_or_even(n):
     """view for /number_odd_or_even/<n> where n is expected to be a number"""
-    value = "even" if n % 2 else "odd"
-    return render_template("6-number_odd_or_even.html", value=value)
+    value = "even" if int(n) % 2 else "odd"
+    return render_template("6-number_odd_or_even.html", n=n, value=value)
 
 
 if __name__ == '__main__':
