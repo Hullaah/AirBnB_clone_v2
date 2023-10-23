93d64395209fe91c6a0fbd3a110f08d9f928dbf3
Hullaah <umaradelo1.247@gmail.com>
2023-10-23 22:14:14 +0100

    implemented the close method foe db_storage and file storage

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application"""
+from models.state import State
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
