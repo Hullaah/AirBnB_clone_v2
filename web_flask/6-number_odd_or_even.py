d0f1a3da71684e05d0c8cc7270836ba29470f67d
Hullaah <umaradelo1.247@gmail.com>
2024-01-02 13:04:15 +0100

    Corrected the tasks

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,7 +44,7 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 
-@app.route("/number_odd_or_even/<n>", strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     """view for /number_odd_or_even/<n> where n is expected to be a number"""
     value = "odd" if int(n) % 2 else "even"
